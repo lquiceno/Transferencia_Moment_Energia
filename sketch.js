52fd9b6cc1a00751490dd432d154700506f60406
lquiceno <60938657+lquiceno@users.noreply.github.com>
2021-04-13 11:22:07 -0500

    Add files via upload
    Este es un la colision, primer cambio

--- sketch.js
+++ sketch.js
@@ -1,89 +1,137 @@
-let suelo;
-let pelota;
-let dt=1/30;
-let gravedad= createVector(0,10);
 
-//Nuevo comentario
+let canvas;
+
+let table;
+let balls = [];
+
+let dt = 1;
+
+let fontsize = 100;
+
+let collisions = 0;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
+  frameRate(200);
 
-  frameRate(30);
+  table = new Table(-400, -250, 800, 500);
 
-  suelo = new Tierra(-windowWidth/2,windowHeight/2-100,windowWidth,100);
+  for(let i = 0; i<12; i++) {
+    balls.push(new Ball(20, createVector(random(-3,3),random(-3,3)), createVector(random(-3,3), random(-3,3))));
 
-  pelota = new Balon(10,20,createVector(-windowWidth/3,-windowHeight/3),createVector(20,-20));
+    for(let j = 0; j < i; j++) {
+      let d = dist(balls[i].pos.x, balls[i].pos.y, balls[j].pos.x, balls[j].pos.y);
+      if (d <= balls[i].r) {
+        balls[i].pos.x += 2*balls[i].r;
+      }
+    }
+  }
+
+  textSize(fontsize);
+	textAlign(LEFT, CENTER);
+
 }
 
-//comentarios
 function draw() {
-  translate(windowWidth/2,windowHeight/2);
-  background(52,152,219);
-  suelo.mostrar();
-  pelota.actVelocidad(gravedad);
-  pelota.movimiento();
-  pelota.mostrar();
+  translate(windowWidth / 2, windowHeight / 2);
+  background(220);
 
-}
 
-//crear tierra
-let Tierra = function (_x, _y, _w, _h) {
+  table.show();
 
-this.x =_x;
-this.y =_y;
-this.w =_w;
-this.h =_h;
-
-this.mostrar = function () {
-  noStroke();
-  fill(211, 84, 0);
-rect(this.x, this.y, this.w, this.h);
-
-}
-
-}
-
-let Balon = function (_mass, _rad, _pos, _vel){
-
-  this.mass=_mass;
-  this.rad=_rad;
-  this.pos=_pos;
-  this.vel=_vel;
-
-  this.trayectoria =[];
-
-  this.mostrar= function (){
-    noStroke();
-    fill(10);
-    ellipse(this.pos.x, this.pos.y, this.rad, this.rad);
-
-    stroke(25);
-
-    for(let i=0;i< this.trayectoria.length-2;i++){
-
-      line(this.trayectoria[i].x,this.trayectoria[i].y,this.trayectoria[i+1].x,this.trayectoria[i+1].y );
+  for (let i = 0; i < balls.length; i++) {
+    for (let j = i; j < balls.length; j++) {
+      if (i !== j) {
+        balls[i].collision(balls[j]);
+      }
     }
+    table.collision(balls[i]);
 
-  }
-
-  this.actVelocidad=function (accel){
-
-    this.vel.x += accel.x *dt;
-    this.vel.y +=accel.y*dt;
-  }
-
-  this.movimiento= function (){
-
-    this.pos.x+=this.vel.x*dt;
-    this.pos.y+=this.vel.y*dt;
-
-    this.trayectoria.push(this.pos.copy());
-    if(this.trayectoria.length > 150) {
-      this.trayectoria.splice(0,1);
-
-    }
-
+    balls[i].update();
+    balls[i].show();
     
   }
 
+  text("Collisions = " + nfc(collisions, 0), 300, -240);
+
+
 }
+
+
+// creation of billar table
+let Table = function (_x, _y, _w, _h) {
+  this.x = _x;
+	this.y = _y;
+	this.w = _w;
+	this.h = _h;
+
+  this.show = function () {
+    noStroke();
+    fill(80);
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  this.collision = function (child) {
+    if ((child.pos.x < this.x +child.r/2) || (child.pos.x > this.x + this.w -child.r/2) ) {
+      child.pos.x -= child.vel.x * dt;
+      child.vel.x *= -1;
+      // collisions += 1;
+    }
+    if ((child.pos.y < this.y +child.r/2) || (child.pos.y > this.y + this.h -child.r/2) ) {
+      child.pos.y -= child.vel.y * dt;
+      child.vel.y *= -1;
+      // collisions += 1;
+    }
+  }
+};
+
+
+// creation of billar ball
+let Ball = function (_r, _pos, _vel) {
+  this.r = _r;
+  this.pos = _pos;
+  this.vel = _vel;
+  this.mass = 1;
+  this.moment = this.mass * this.vel;
+  this.energy = 0.5*this.mass*this.vel.magSq();
+
+  this.show = function() {
+    noStroke();
+    fill(180);
+    ellipse(this.pos.x, this.pos.y, this.r, this.r);
+  }
+
+  this.update = function () {
+    // update the position
+    this.pos.x += this.vel.x * dt;
+    this.pos.y += this.vel.y * dt;
+
+    this.energy = 0.5*this.mass*this.vel.magSq();
+  }
+
+  this.collision = function (child) {
+    let d = dist(this.pos.x, this.pos.y, child.pos.x, child.pos.y);
+    let dv = this.pos.copy().sub(child.pos); 
+
+    let en = (this.energy + child.energy) / 2;
+
+    if (d < (this.r/2 + child.r/2 + (0.1 * child.r/2))) {
+      let newMag = sqrt((2*en)/this.mass);
+      
+      
+      this.vel.reflect(dv);
+      this.vel.setMag(newMag);
+      this.pos.x += 1*this.vel.x * dt;
+      this.pos.y += 1*this.vel.y * dt;
+
+      
+       child.vel.reflect(dv);
+       child.vel.setMag(newMag);
+       child.pos.x += 1*child.vel.x * dt;
+      child.pos.y += 1*child.vel.y * dt;
+
+       collisions += 0.5;
+    }
+  }
+}
+
