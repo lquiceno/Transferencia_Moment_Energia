640eb1969fe1bbdb70fadc0b332c43b88e31912e
voviedo13 <62526183+voviedo13@users.noreply.github.com>
2021-04-22 11:12:15 -0500

    Add files via upload

--- table.js
+++ table.js
@@ -1,9 +1,7 @@
 let dt = 1/10;
-//culo
 //si phi=80; //porcentaje de elasticidad 
 //choque completamente elastico phi=100, choque completamente inelastico phi=0
 //Modificacion del main
-//Electroencefalografista
 
 let balls=[]; //arreglo vacio bolas
 //si N=4; cuatro bolas 
@@ -11,7 +9,6 @@ let l=100; //altura para mostrar botones
 //crea pantalla
 let w=window.innerWidth;
 let h=window.innerHeight-l; 
-
 
 
 function setup() {
@@ -22,14 +19,14 @@ button=createButton('Restaurar');
 button.mousePressed(resetSketch);
 
 //barra de bolas
-p1=createP('Numero de Bolas');
+p1=createP('Número de bolas');
 p1.position(w/3+25,h+l/5); //posicion barra
 
 //barra elasticidad
-p2=createP('Porcentaje de Elasticidad');
+p2=createP('Porcentaje de elasticidad');
 p2.position(2*w/3,h+l/5); //posicion barra
 
-sliderx = createSlider(0, 6, 3); //controla numero de bolas, si el usuario crea mas, las crea del mismo color
+sliderx = createSlider(0, 6, 3); //controla numero de bolas
 sliderx.position(w/3, h+ l/2); //posicion
 sliderx.style('width', '150px'); //color
 
@@ -37,10 +34,19 @@ slidery = createSlider(0.1, 100, 50);
 slidery.position(2*w/3, h+ l/2);
 slidery.style('width', '150px');
 
+//barra fricción
+p3 = createP('Coef. de fricción');
+p3.position(2*w/3-320,h+l/5);
+sliderz = createSlider(0, 1, 1, 0.01);
+sliderz.style('width', '150px');
+
+
 sliderx.changed(resetSketch);
 slidery.changed(resetSketch);
+sliderz.changed(resetSketch);
 
 resetSketch();
+
 }
 
 function resetSketch() {
@@ -48,6 +54,7 @@ function resetSketch() {
   balls=[]; //bolas
   N=sliderx.value(); //numero bolas
   phi=slidery.value(); //porcentaje ealsticidad
+  b=sliderz.value(); //coef. de fricción
   //crea las bolas y las pone en lugar aleatorio
   for(let i=0; i<N;i++){
   balls.push(new ball(i, random(1,10),random(10,20),createVector(random(-w/2+50,w/2-50),random(-h/2+50,h/2-50)),createVector(random(-50,50),random(-50,50))));
@@ -112,6 +119,7 @@ let ball = function(i, _mass, _rad, _pos, _vel){
       case 5:
         fill(255,177,187);
         break;
+        //se acaba la función
     }
     
     ellipse(this.pos.x, this.pos.y, 2*this.radio, 2*this.radio); //en realidad ellipse toma el ancho y alto total de la elipse, en este caso sería el diametro
@@ -119,8 +127,16 @@ let ball = function(i, _mass, _rad, _pos, _vel){
   }
 //funcion movimiento la cual cambia posicion usando ec. Cinematica x = vt
   this.movimiento = function(){
+
+
+    //b = 0.2; //coef. de fricción
+    beta = b*dt/this.mass; //fricción si b diferente de 0
+    this.vel.x = (1.0 - beta)*this.vel.x;
+    this.vel.y = (1.0 - beta)*this.vel.y;
+
     this.pos.x += this.vel.x*dt;
     this.pos.y += this.vel.y*dt;
+
   }
 //funcion colision que cambia velocidad
   this.collision=function(){
@@ -130,19 +146,11 @@ this.vel.x*=-1;
 if ((this.pos.y<-h/2+10+this.radio) || (this.pos.y>h/2-10-this.radio)){
   this.vel.y*=-1;
   }
-
   }
 
 }
 
-
-
-
 inelasticballscollision=function(object1,object2){
-
-  
-
-
 let d=dist(object1.pos.x,object1.pos.y,object2.pos.x,object2.pos.y);
 let u= createVector((object1.pos.x-object2.pos.x)/(d),(object1.pos.y-object2.pos.y)/(d));
 let s= createVector((object1.vel.x-object2.vel.x)/(dist(object1.vel.x,object1.vel.y,object2.vel.x,object2.vel.y)),(object1.vel.y-object2.vel.y)/(dist(object1.vel.x,object1.vel.y,object2.vel.x,object2.vel.y)));
@@ -153,18 +161,15 @@ let B=2*((object1.mass+object2.mass)/(object1.mass*object2.mass))*(((object1.vel
 let C=(1-sq(k))*sq(dist(object1.vel.x,object1.vel.y,object2.vel.x,object2.vel.y));
 let D=sq(B)-4*A*C;
 if(D<0){
-  
   object1.vel.x=0;
   object1.vel.y=0;
   object2.vel.x=0;
   object2.vel.y=0;
-
 }else{
 let a1=(-B+sqrt(sq(B)-4*A*C))/(2*A);
 let a2=(-B-sqrt(sq(B)-4*A*C))/(2*A); 
 let a=max(a1,a2);
  
-  
 object1.vel.x+=(a/object1.mass)*u.x;
 object1.vel.y+=(a/object1.mass)*u.y;
 object2.vel.x-=(a/object2.mass)*u.x;
@@ -174,7 +179,6 @@ object2.vel.y-=(a/object2.mass)*u.y;
 }
 
 
-
 let border = function(){
   //funcion mostrar para aparecer el lienzo 
   this.mostrar = function() {
@@ -182,10 +186,7 @@ let border = function(){
     fill(0,143,57);
     rect(-w/2+10,-h/2+10,2*(w/2-10),2*(h/2-10));
     //stroke(100);
-
   }
 }
-
-
 
   
